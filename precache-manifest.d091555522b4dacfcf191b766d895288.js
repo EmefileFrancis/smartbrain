91ec963afa9b56f5daacaa1c96ba71f3863cd3f7
Emefile Francis <emefilefranklin@gmail.com>
2020-02-12 16:30:37 +0100

    Updates

---
+++ precache-manifest.d091555522b4dacfcf191b766d895288.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d40d448c765f747af3ce095ad18555",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "8840c843cd01a91519ef",
+    "url": "/smartbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4bb5323f45bd0f5626ea",
+    "url": "/smartbrain/static/css/main.b685d0d2.chunk.css"
+  },
+  {
+    "revision": "8840c843cd01a91519ef",
+    "url": "/smartbrain/static/js/2.2b6d35fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smartbrain/static/js/2.2b6d35fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bb5323f45bd0f5626ea",
+    "url": "/smartbrain/static/js/main.95ef4da5.chunk.js"
+  },
+  {
+    "revision": "9edabdfe5fc8eccd50e6",
+    "url": "/smartbrain/static/js/runtime-main.7e72c5df.js"
+  }
+]);
